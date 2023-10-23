85089dd05923effd070130f2ffeaba34fb332329
Erdem <zahrakon06@gmail.com>
2023-10-23 14:54:05 +0300

    Update SertificateCard.svelte

--- src/routes/education/components/SertificateCard.svelte
+++ src/routes/education/components/SertificateCard.svelte
@@ -5,6 +5,8 @@
 
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="card" on:click={onClick}>
   <div class="top">
     <img src={image} alt={name} />
